ce4dbc380da95f02b17551d7415aa6007a54c624
Yu Zhou <junior147147@yahoo.com>
2018-04-14 23:55:47 -0700

    finished in terms of coding

--- scrape.py
+++ scrape.py
@@ -1,35 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
-with open('blog.html') as html_file:
-    soup = BeautifulSoup(html_file, 'lxml')
+src = requests.get('https://floating-crag-22061.herokuapp.com/blogs').text
+soup = BeautifulSoup(src, 'lxml')
 
-content = {
-        'quote' : '',
-        'introduction' : '',
-        'p.s.?' : ''
-}
+csv_file = open('blog_content.csv','w')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Headline', 'Date', 'Summary', 'Blog Link'])
 
 content_container = soup.find('div', class_= "twelve wide computer twelve wide tablet column main text")
-# article = content_container.find('div', class_='item')
-
-counter = 1
-print('\n')
-print('\n')
-
 for article in content_container.find_all('div', class_='item'):
     content = article.find('div', class_='content')
-
-    content_title = content.find('a', class_='header').text
+    content_headline = content.find('a', class_='header').text
     content_date = content.find('div', class_='meta').span.text
-    content_description = content.find('div', class_='description').p.text
-
-    print('----------Article ' + str(counter) + '----------')
-    print(content_title)
-    print(content_date)
-    print(content_description)
-    print('\n')
-    counter += 1
-
-# readmore_link = article.find('a', class_="ui medium image fluid image-fixed")['href']
-# print(readmore_link)
+    content_summary = content.find('div', class_='description').p.text
+    content_readmore = 'https://floating-crag-22061.herokuapp.com' + article.find('a', class_="ui medium image fluid image-fixed")['href']
+    csv_writer.writerow([content_headline, content_date, content_summary, content_readmore])
+csv_file.close()
