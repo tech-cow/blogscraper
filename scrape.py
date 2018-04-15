ebdc61431d8e6d56ddc50ee9223349e340186a62
Yu Zhou <junior147147@yahoo.com>
2018-04-14 22:08:52 -0700

    e

--- scrape.py
+++ scrape.py
@@ -4,9 +4,11 @@ import requests
 with open('blog.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-# print(soup.prettify())
+content = {
+        'Quote' : '',
+        'Introduction' : '',
+        'p.s.?' : ''
+}
 
-title = soup.title.text
-
-item = soup.find('div', class_='item')
-print(item.prettify())
+for key, val in content.items():
+    print(key + ':' + val)
